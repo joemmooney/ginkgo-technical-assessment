44c0d775d58d07f7d95834796e58743ace7db5df
Joseph Mooney <josephmooney@Josephs-MacBook-2.local>
2021-01-24 10:40:07 -0500

    Added django-q support

--- backend/urls.py
+++ backend/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from rest_framework import routers
 from searchproteins import views
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'SearchProteins', views.SearchProteinsView, 'SearchProteins')
@@ -24,4 +25,5 @@ router.register(r'SearchProteins', views.SearchProteinsView, 'SearchProteins')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
